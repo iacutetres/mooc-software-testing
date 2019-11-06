0716ce53501af06bbfb53c2fc0723bfcbd15b4db
Quique Martinez <quique@MacBook-Pro-de-Quique.local>
2019-11-07 00:03:44 +0100

    pract1

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,17 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "text={0}, result={1}")
+    @CsvSource({ "gxx,false","ggxx,true","xxggxx,true", "xxgxx,false"
+            , "xxggyygxx,false", "xxggyygg,true" })
+    public void testAlgorithm(String text, boolean  expectedResult) {
+        GHappy bags = new GHappy();
+        boolean result = bags.gHappy(text);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
