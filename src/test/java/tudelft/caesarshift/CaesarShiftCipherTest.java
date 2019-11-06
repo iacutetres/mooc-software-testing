478f865e1c3ef44137c82cfe45183a5619666ec2
Quique Martinez <quique@MacBook-Pro-de-Quique.local>
2019-11-07 00:17:26 +0100

    pract2

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,15 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "{0}+{1}={2}")
+    @CsvSource({ "abc,3,def","abc,0,abc","xyz,3,abc","abc3,3,invalid" })
+    public void testAlgorithm(String text,int shift, String  expectedResult) {
+        CaesarShiftCipher bags = new CaesarShiftCipher();
+        String result = bags.CaesarShiftCipher(text,shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
